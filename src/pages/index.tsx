3e45106d901a6711f79cf8c2515c1ecca5da7a22
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-07-31 11:36:09 +0100

    homepage layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,11 @@
 import Image from 'next/image'
-import { Inter } from 'next/font/google'
 import Layout from '@/components/Layout'
-
-const inter = Inter({ subsets: ['latin'] })
+import NoteList from '@/components/NoteList'
 
 export default function Home() {
   return (
     <Layout>
-      <h1>Home</h1>
+      <NoteList />
     </Layout>
   )
 }
