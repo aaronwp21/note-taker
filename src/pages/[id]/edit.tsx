6c6f22455a05d4d47ed68f143c1e63a49aac13f7
aaronwp21 <110860467+aaronwp21@users.noreply.github.com>
2023-08-09 00:36:15 +0100

    redirect on missing user

--- src/pages/[id]/edit.tsx
+++ src/pages/[id]/edit.tsx
@@ -1,4 +1,6 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useUser } from '@auth0/nextjs-auth0/client';
 import { DashboardContext } from '@/components/contexts/dashboard.context';
 import Layout from '@/components/Layout';
 import NoteLayout from '@/components/NoteLayout';
@@ -6,14 +8,28 @@ import { Note } from '@/types';
 import EditNote from '@/components/EditNote';
 import Head from 'next/head';
 
+
 type NoteLayoutProps = {
   notes: Note[];
 };
 
 export default function Edit() {
+  const router = useRouter();
   const [currentNote, setCurrentNote] = useState<Note | undefined | null>(null);
 
-  const { notes, onUpdateNote, onAddTag, availableTags } = useContext(DashboardContext)
+  const { notes, onUpdateNote, onAddTag, availableTags } = useContext(DashboardContext);
+
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/');
+    }
+  }, [router, user]);
+
+  if (!user) {
+    return null;
+  }
 
   return (
     <>
--- src/pages/new.tsx
+++ src/pages/new.tsx
@@ -1,13 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import Head from 'next/head';
+import { useRouter } from 'next/dist/client/router';
+import { useUser } from '@auth0/nextjs-auth0/client';
 import Layout from '@/components/Layout';
 import NewNote from '@/components/NewNote';
 import { DashboardContext } from '@/components/contexts/dashboard.context';
 
-
 export default function New() {
+  const router = useRouter();
   const { onCreateNote, onAddTag, availableTags } =
     useContext(DashboardContext);
+
+  const { user } = useUser();
+
+  useEffect(() => {
+    if (!user) {
+      router.push('/');
+    }
+  }, [router, user]);
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <>
       <Head>
